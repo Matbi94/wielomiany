70bd02298334e92572376dfcc2a18cc36cd08ea3
unknown <you@example.com>
2015-11-07 17:18:02 +0100

    first commit

--- Main.java
+++ Main.java
@@ -6,10 +6,19 @@ public class Main {
 	// write your code here
         int a=2,b=-3,c=-8;
         double delta=b*b-4*a*c;
-        double x1=-b+Math.sqrt(delta)/2*a;
-        double x2=-b-Math.sqrt(delta)/2*a;
-        System.out.println(x1);
-        System.out.println(x2);
+        if(delta>0) {
+            double x1 = -b + Math.sqrt(delta) / 2 * a;
+            double x2 = -b - Math.sqrt(delta) / 2 * a;
+            System.out.println(x1);
+            System.out.println(x2);
+        }
+        else if(delta<0) {
+            System.out.println("nie istnieje");
+        }
+        else if(delta==0){
+            double x0=-b/2*a;
+            System.out.println(x0);
+        }
 
 
 
